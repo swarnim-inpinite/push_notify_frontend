0db3b4b9e6ea353a4ad63fca7d7c9de81ac15717
Swarnim Dhawane <Swarnim@gmail.com>
2024-04-23 15:13:50 +0530

    Updated Code 2.0

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,14 @@
+// firebase-messaging-sw.js
+
 self.addEventListener('push', function(event) {
+  const title = 'New Event Notification';
+  const body = 'A new event has been added!';
+  
   const options = {
-    body: event.data.text(),
-    icon: 'icons/icon-192x192.png', // Update with your actual icon path
+    body: body,
+    icon: 'cal_icon.png', // Update with your actual icon path
     badge: 'path/to/badge.png' // Update with your actual badge path
   };
-  event.waitUntil(self.registration.showNotification('Notification', options));
+  
+  event.waitUntil(self.registration.showNotification(title, options));
 });
