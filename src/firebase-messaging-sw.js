c8fc7f589822d8fd67ee36d07f1a9502affb4fd3
Swarnim Dhawane <Swarnim@gmail.com>
2024-04-24 00:24:30 +0530

    Updated Code 2.4

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,13 +1,14 @@
 // firebase-messaging-sw.js
 
 self.addEventListener('push', function(event) {
+  console.log('Push event received:', event);
   const title = 'New Event Notification Firebase';
   const body = 'A new event has been added!';
   
   const options = {
     body: body,
     icon: 'cal_icon.png', 
-    badge: 'path/to/badge.png' // Update with your actual badge path
+    badge: 'path/to/badge.png' 
   };
   
   event.waitUntil(self.registration.showNotification(title, options));
