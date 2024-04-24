169c357d300e9085085f1fedb5c18125c864644b
Swarnim Dhawane <Swarnim@gmail.com>
2024-04-24 18:59:39 +0530

    Updated Code 3.3

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getMessaging, getToken } from "firebase/messaging";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -49,6 +49,8 @@ export const generatetoken = async () => {
   }
  
 }
+
+
 
 
 
