96bd0f22f7f54de72957367e6a080f6a3dca546a
Swarnim Dhawane <Swarnim@gmail.com>
2024-04-25 01:06:55 +0530

    Updated Code 4.0

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,26 @@
 self.addEventListener('push', function(event) {
-  console.log("evvent is");
-  const options = {
-    body: event.data.text(),
-    icon: 'icons/icon-192x192.png', // Update with your actual icon path
-    badge: 'path/to/badge.png' // Update with your actual badge path
-  };
-  event.waitUntil(self.registration.showNotification('Notification-Title', options));
+  // Extract notification data from the event
+  console.log("eventis", event);
+  const data = event.data.json();
+
+  // Display notifications for each user
+  sendPushNotifications(data.usersInfo)
+    .then((notifications) => {
+      // Display each notification
+      notifications.forEach((notification) => {
+        // Display the notification using showNotification
+        self.registration.showNotification(notification.title, {
+          body: notification.body,
+          // Update with your actual icon and badge paths
+          icon: '/path/to/icon.png',
+          badge: '/path/to/badge.png'
+        });
+      });
+    })
+    .catch((error) => {
+      console.error('Error sending push notifications:', error);
+    });
+
+  // Tell the service worker to keep the event alive until the notifications are displayed
+  event.waitUntil(Promise.resolve());
 });
