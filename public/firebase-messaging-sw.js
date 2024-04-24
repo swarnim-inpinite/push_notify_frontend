15f8e90dd4158b577d0a58c731ec5363ab88f645
Swarnim Dhawane <Swarnim@gmail.com>
2024-04-24 23:52:10 +0530

    Updated Code 3.6

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -9,3 +9,13 @@ messaging.onMessage((payload) => {
         icon: '/path/to/icon.png' // Specify the path to your notification icon
     });
 });
+
+
+self.addEventListener('push', function(event) {
+  const options = {
+    body: event.data.text(),
+    icon: 'icons/icon-192x192.png', // Update with your actual icon path
+    badge: 'path/to/badge.png' // Update with your actual badge path
+  };
+  event.waitUntil(self.registration.showNotification('Notification', options));
+});
